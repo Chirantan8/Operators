e5c38b99f83a3a6512b6d374e7cca54889ad4ef5
Chirantan8 <163657893+Chirantan8@users.noreply.github.com>
2024-08-26 12:52:40 +0530

    Add files via upload

---
+++ Operators/program9.java
@@ -0,0 +1,8 @@
+class Program9{
+	public static void main(String[] args){
+		
+		System.out.println(25^6);
+	}
+}
+
+
---
+++ Operators/program9b.java
@@ -0,0 +1,6 @@
+class Program9b{
+	public static void main(String[] args){
+
+		System.out.println(9>>>1);
+	}
+}
